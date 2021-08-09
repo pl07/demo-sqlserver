5db11ef1bb7af0e54498b6366cf62030c9a8fa96
pl07 <815192806@qq.com>
2021-08-09 16:39:37 +0800

    First commit

---
+++ src/main/java/com/example/demo/boot/mysql/entity/MyUser.java
@@ -0,0 +1,42 @@
+package com.example.demo.boot.mysql.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+// user 是SQL的关键字，这里class name不能为 User
+public class MyUser {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
